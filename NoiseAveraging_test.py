423615e08acedeea754c4b4e198dd595c1158e13
Cameron King <cameron@obelisk.physics.wisc.edu>
2018-07-12 09:59:51 -0500

    Performing corrections to the test code

--- NoiseAveraging_test.py
+++ NoiseAveraging_test.py
@@ -5,14 +5,21 @@ import numpy as np
 
 import NoiseAveraging
 
+@pytest.fixture
+def noise_sample():
+    import numpy as np
+    orig = np.arange(0, 12, 2)
+    interleave = np.arange(1, 11, 2)
+    full = np.arange(0, 12, 1)
+    return orig, interleave, full
+
 
 class Test_Doubling_function(object):
-    original_test = np.arange(0, 12, 2)
-    new_samples, full_sampling = NoiseAveraging.noise_doubling(original_test)
 
-    def test_noise_doubling(self, new_samples):
-        assert new_samples == np.arange(1, 11, 2)
+    def test_noise_doubling(self, noise_sample):
+        import numpy as np
 
-
-    def test_noise_doubling_full_array(self, full_sampling):
-        assert full_sampling == np.arange(0, 12, 1)
+        orig, interleave, full = noise_sample
+        new_samples, full_sampling = NoiseAveraging.noise_doubling(orig)
+        assert np.array_equal(new_samples, interleave)
+        assert np.array_equal(full, full_sampling)
