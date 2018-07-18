28c63d806c3ef58a24f02677e9594e0be43d8b09
Cameron King <ceking5@wisc.edu>
2018-07-18 17:14:21 -0500

    Changed convergence to use process fidelity (a good metric)

--- NoiseAveraging.py
+++ NoiseAveraging.py
@@ -85,7 +85,7 @@ def simple_noise_sampling(tfinal, samples0=15):
         cj_array1[:, :, ::2] = cj_array0
         cj_array1[:, :, 1::2] = cj_array1new
         average_cj1 = noise_averaging(x1full, noise_samples1full, cj_array1)
-        converge_value = np.linalg.norm(average_cj0 - average_cj1)
+        converge_value = qmf.processInfidelity(average_cj0, average_cj1)
 
         print(converge_value)
 
