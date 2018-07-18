89f8004a5f39ef970fff63d7b9379ff438e3839a
Cameron King <cameron@obelisk.physics.wisc.edu>
2018-07-18 10:12:21 -0500

    Updated based on errors

--- CJFidelities.py
+++ CJFidelities.py
@@ -21,7 +21,7 @@ class CJ (object):
         """
         dim = hamiltonian.shape[0]
         norm = 1.0 / float(len(indices))
-        converted_indices = int((dim + 1) * indices)
+        converted_indices = int((dim + 1))) * indices
         chi0 = np.zeros((dim**2, dim**2), dtype=complex)
         self.chi0 = chi0[np.ix_(converted_indices, converted_indices)] = norm
         self.kernel = np.kron(np.identity(dim), hamiltonian)
