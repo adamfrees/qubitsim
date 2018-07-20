575f5ff5cf11dc922f70b712a05b16e3fdb95917
Cameron King <cameron@obelisk.physics.wisc.edu>
2018-07-20 10:46:46 -0500

    Updated noise doubling test

--- NoiseAveraging_test.py
+++ NoiseAveraging_test.py
@@ -5,21 +5,12 @@ import numpy as np
 
 import NoiseAveraging
 
-@pytest.fixture
-def noise_sample():
+def test_noise_doubling():
     import numpy as np
-    orig = np.arange(0, 12, 2)
-    interleave = np.arange(1, 11, 2)
+
+    orig = np.arange(0, 11, 2)
+    interleave = np.arange(1, 10, 2)
     full = np.arange(0, 11, 1)
-    return orig, interleave, full
-
-
-class Test_Doubling_function(object):
-
-    def test_noise_doubling(self, noise_sample):
-        import numpy as np
-
-        orig, interleave, full = noise_sample
-        new_samples, full_sampling = NoiseAveraging.noise_doubling(orig)
-        assert np.array_equal(new_samples, interleave)
-        assert np.array_equal(full, full_sampling)
+    new_samples, full_sampling = NoiseAveraging.noise_doubling(orig)
+    assert np.array_equal(new_samples, interleave)
+    assert np.array_equal(full, full_sampling)
