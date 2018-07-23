e25f164607af55db5ab4dd0b0d43f3c8f7a3e0be
Cameron King <cameron@obelisk.physics.wisc.edu>
2018-07-23 14:18:45 -0500

    Noise-free time evolution code

--- NoiselessEvolution.py
+++ NoiselessEvolution.py
@@ -12,4 +12,18 @@ def time_evolution_RF(tfinal, arch='hybrid'):
         qubit = hybrid.SOSSHybrid(operating_point, match_freq)
         H0 = qubit.hamiltonian_lab()
 
-        ChoiSimulation
+        ChoiSimulation = CJFidelities.CJ(indices, H0, np.zeros((3,3)))
+        if tfinal == 0:
+            return ChoiSimulation.chi0
+        else:
+            return ChoiSimulation.chi_final_RF(tfinal)
+
+if __name__ == '__main__':
+    tsteps = 100
+    trange = np.linspace(0, 30, tsteps)
+    cj_time_array = np.zeros((9, 9, tsteps), dtype=complex)
+    for i in range(tsteps):
+        cj_time_array[i] += time_evolution_RF(trange[i])
+    np.save('trange_noiseless_test.npy', trange)
+    np.save('cj_array_noiseless_test.npy', cj_time_array)
+
