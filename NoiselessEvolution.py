0c7f9963f329180d39a01069953bdf7a2ada069d
Cameron King <cameron@obelisk.physics.wisc.edu>
2018-07-24 14:31:36 -0500

    Added code for generic hybrid qubit

--- NoiselessEvolution.py
+++ NoiselessEvolution.py
@@ -3,13 +3,26 @@
 import numpy as np
 import CJFidelities
 
-def time_evolution_RF(tfinal, arch='hybrid'):
-    if arch == 'hybrid':
+def time_evolution_RF(tfinal, arch='hybrid-SOSS'):
+    if arch == 'hybrid-SOSS':
         import HybridQubit as hybrid
         indices = [0, 1]
         match_freq = 10.0
         operating_point = 3.5
         qubit = hybrid.SOSSHybrid(operating_point, match_freq)
+        H0 = qubit.hamiltonian_lab()
+
+        ChoiSimulation = CJFidelities.CJ(indices, H0, np.zeros((3,3)))
+        if tfinal == 0:
+            return ChoiSimulation.chi0
+        else:
+            return ChoiSimulation.chi_final_RF(tfinal)
+    else:
+        import HybridQubit as hybrid
+        indices = [0, 1]
+        match_freq = 10.0
+        operating_point = 1.0
+        qubit = hybrid.HybridQubit(operating_point * match_freq, match_freq, 0.7 * match_freq, 0.7*match_freq)
         H0 = qubit.hamiltonian_lab()
 
         ChoiSimulation = CJFidelities.CJ(indices, H0, np.zeros((3,3)))
