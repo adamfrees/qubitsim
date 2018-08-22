b1c54f6e8eecb1298a51cecb4270653b763bc432
Cameron King <cameron@Camerons-MacBook-Pro.local>
2018-08-21 22:32:30 -0500

    Updating function

--- OperatingPointSelection.py
+++ OperatingPointSelection.py
@@ -6,8 +6,79 @@ import numpy as np
 import scipy.linalg as LA
 
 import HybridQubit as hybrid
-import NoiseAveraging as noise
+import CJFidelities as cj
 
+
+def two_sigma_doubling(original, sigma):
+    middle = np.nonzero(np.fabs(original) <= 2*sigma)[0]
+    new_size = len(original) + len(middle) - 1
+    start = range(0, middle[0])
+    finish = range(middle[-1]+1, len(original))
+    new_start = start
+    new_middle = range(middle[0], middle[0]+2*len(middle)-1)
+    new_finish = range(new_middle[-1]+1, new_size)
+    new_samples = noise_doubling(original[middle])[1]
+
+    new_array = np.zeros((new_size))
+    new_array[new_start] += original[start]
+    new_array[new_middle] += new_samples
+    new_array[new_finish] += original[finish]
+    return new_array
+
+
+def wing_doubling(original, sigma):
+    middle = np.nonzero(np.fabs(original) <= 2*sigma)[0]
+    start = range(0, middle[0])
+    finish = range(middle[-1]+1, len(original))
+    new_size = len(original) + len(start) + len(finish) - 2
+
+    start_double = noise_doubling(original[start])[1]
+    finish_double = noise_doubling(original[finish])[1]
+
+    new_array = np.zeros((new_size))
+    new_array[0:len(start_double)] += start_double
+    new_array[len(start_double):len(start_double)+len(middle)] += original[middle]
+    new_array[len(start_double)+len(middle):] += finish_double
+    return new_array
+
+
+def noise_sample(qubit, tfinal, ded):
+    H0 = np.diag(qubit.energies())
+    noise = qubit.detuning_noise_qubit(ded)
+    indices = [0, 1]
+
+    ChoiSimulation = cj.CJ(indices, H0, noise)
+    if tfinal == 0:
+        return ChoiSimulation.chi0
+    else:
+        return ChoiSimulation.chi_final_RF(tfinal)
+
+def noise_iteration(qubit, tfinal, noise_samples):
+    cj_array = np.zeros((9, 9, len(noise_samples), dtype=complex)
+    for i in range(len(noise_samples)):
+        ded = noise_samples[i]
+        cj_array[:, :, i] += noise_sample(qubit, tfinal, ded)
+    return cj_array
+
+
+def noise_averaging(x, noise_weights, cj_array):
+    from scipy.integrate import simps
+    norm = simps(noise_weights, x)
+    matrix_int = simps(np.multiply(cj_array, noise_weights), x)
+    return matrix_int / norm
+
+
+def multi_sigma_noise_sampling(qubit, tfinal, sigma_array):
+    from scipy.stats import norm
+    noise_samples0 = np.linspace(-5*sigma_array[-1], 5*sigma_array[-1], 101)
+    noise_weights = np.zeros((len(sigma_array), len(noise_samples0)))
+    for i in range(len(sigma_array)):
+        noise_weights[i, :] += norm.pdf(noise_samples0, loc=0.0, scale=sigma_array[i])
+    
+    converge_value = 1.0
+    num_runs = 1
+
+    cj_array0 = noise_iteration(qubit, tfinal, noise_samples0)
 
 def multi_sigma_convergence(tfinal, sigma_array, qubit):
     """Ensure convergence of averaging a computed chi-matrix array with 
