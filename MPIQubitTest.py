dfe6b4ae0202958a45b51d3a7bd6b4e083d9c685
Cameron King <cameron@obelisk.physics.wisc.edu>
2018-09-04 09:53:26 -0500

    Testing riffle distribution

--- MPIQubitTest.py
+++ MPIQubitTest.py
@@ -10,13 +10,12 @@ rank = comm.Get_rank()
 
 data = None
 if rank == 0:
-    data = np.linspace(0.5, 5.5, 3*size)
+    data0 = np.linspace(0.5, 5.5, size)
+    data1 = np.linspace(0.98, 1.02, 5)
+    data2 = np.linspace(0.95, 1.05, 11)
+    data = np.flatten(np.hstack((data0, data1, data2)))
 
 recvbuf = np.empty((3), dtype=float)
 comm.Scatter(data, recvbuf, root=0)
 
-for i in range(len(recvbuf)):
-    qubit = hybrid.SOSSHybrid(recvbuf[i], 10.0)
-    print('The rank of the worker is {}'.format(rank))
-    print('The sent detuning point is {}'.format(recvbuf[i]))
-    print('The qubit detuning is {}'.format(qubit.ed / qubit.stsplitting))
+print(rank, recvbuf)
