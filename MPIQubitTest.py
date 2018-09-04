ab4a76e1bafba5697a9a446e72c4eede204bcadb
Cameron King <cameron@obelisk.physics.wisc.edu>
2018-09-04 10:44:56 -0500

    Correct version to scatter the numpy parameters

--- MPIQubitTest.py
+++ MPIQubitTest.py
@@ -10,12 +10,11 @@ rank = comm.Get_rank()
 
 data = None
 if rank == 0:
-    data0 = np.linspace(0.5, 5.5, size)
-    data1 = np.linspace(0.98, 1.02, 5)
-    data2 = np.linspace(0.95, 1.05, 11)
-    data = np.flatten(np.hstack((data0, data1, data2)))
+    data0 = np.linspace(0.5, 5.5, size, dtype=float)
+    data1 = np.linspace(0.98, 1.02, 5, dtype=float)
+    data2 = np.linspace(0.95, 1.05, 11, dtype=float)
+    data = np.ravel(np.array(np.meshgrid(data0, data1, data2)).T)
 
-recvbuf = np.empty((3), dtype=float)
+recvbuf = np.empty((3*5*11), dtype=float)
 comm.Scatter(data, recvbuf, root=0)
-
 print(rank, recvbuf)
