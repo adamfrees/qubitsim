d838ba92252a363cc8dd77d63b1233fa9f81eefb
Cameron King <cameron@Camerons-MacBook-Pro.local>
2018-09-05 10:33:18 -0500

    Incorrect variable placement correction

--- OperatingPointSelection.py
+++ OperatingPointSelection.py
@@ -212,8 +212,8 @@ def time_evolution_point(operating_point, delta1_point, delta2_point):
     stsplitting_ref = qubit_base.stsplitting
     ed_ref = qubit_base.ed
     qubit = hybrid.HybridQubit(ed_ref,
+                               stsplitting_ref
                                delta1_point * delta1_ref,
-                               delta2_point * delta2_ref,
-                               stsplitting_ref)
+                               delta2_point * delta2_ref)
     trange, sigma_array, mass_chi_array = time_sweep(qubit)
     return trange, sigma_array, mass_chi_array
