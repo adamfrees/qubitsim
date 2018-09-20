60773896761925a81f18a4ef69747e079972735c
Cameron King <cameron@Camerons-MacBook-Pro.local>
2018-09-20 14:36:53 -0500

    Adding requirements for python packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='qubitsim',
+    version='0.1dev',
+    packages=['qubitsim',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
