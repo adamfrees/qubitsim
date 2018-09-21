1948ad9f981e9585264eb130a180b26716838825
Cameron King <cameron@Camerons-MacBook-Pro.local>
2018-09-21 15:40:11 -0500

    Starting to define class

---
+++ qubitsim/qubit/ChargeQuadrupole/ChargeQuadrupole.py
@@ -0,0 +1,31 @@
+# Source code for the charge quadrupole qubit class
+
+import math
+import numpy as np
+import scipy.linalg as LA
+
+class CQ(object):
+    """
+    Return a charge quadrupole qubit object
+    """
+    def __init__(self, eq, ed, delta1, delta2):
+        """
+        Initilizes the qubit
+        Inputs:
+          eq: quadrupolar detuning in GHz
+          delta: tunnel coupling in GHz
+        """
+        self.eq = eq
+        self.ed = ed
+        self.delta1 = delta1
+        self.delta2 = delta2
+        self.dim = 3
+    
+    def hamiltonian_lab(self):
+        """
+        Returns the hamiltonian in the labframe in units of radians/ns
+        """
+        H0 = np.array([0.5 * self.eq, (self.delta1 + self.delta2)/math.sqrt(2), (self.delta1 - self.delta2)/math.sqrt(2)],
+                      [(self.delta1 + self.delta2)/math.sqrt(2), -0.5 * self.eq, self.ed],
+                      [(self.delta1 - self.delta2)/math.sqrt(2), self.ed, 0.5*self.eq]])
+        return 2 * math.pi * H0
