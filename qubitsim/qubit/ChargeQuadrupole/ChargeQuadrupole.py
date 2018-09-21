de6976002e969d4248b8988c481862d1f2b1c721
Cameron King <cameron@Camerons-MacBook-Pro.local>
2018-09-21 15:49:36 -0500

    Adding helper functions for qubit simulations

--- qubitsim/qubit/ChargeQuadrupole/ChargeQuadrupole.py
+++ qubitsim/qubit/ChargeQuadrupole/ChargeQuadrupole.py
@@ -21,14 +21,52 @@ class CQ(object):
         self.delta2 = delta2
         self.dim = 3
     
+
     def hamiltonian_lab(self):
         """
-        Returns the hamiltonian in the labframe in units of radians/ns
+        Returns the hamiltonian in the labframe in units of rad/ns
         """
         H0 = np.array([[0.5 * self.eq, (self.delta1 + self.delta2)/math.sqrt(2), (self.delta1 - self.delta2)/math.sqrt(2)],
                       [(self.delta1 + self.delta2)/math.sqrt(2), -0.5 * self.eq, self.ed],
                       [(self.delta1 - self.delta2)/math.sqrt(2), self.ed, 0.5 * self.eq]])
         return 2 * math.pi * H0
+
+
+    def dim_sort(self):
+        """
+        The cq qubit has an odd energy structure, this gives 
+        the indices to sort eigenvectors and eigenvalues into 
+        qubit + leakage ordering
+        """
+        return [0, 2, 1]
+
+
+    def energies(self):
+        """
+        Return an array of system energies in rad/ns
+        """
+        return LA.eigvalsh(self.hamiltonian_lab())[self.dim_sort()]
+
+
+    def qubit_splitting(self):
+        """
+        Return the qubit splitting in rad/ns
+        """
+        evals = self.energies()
+        return evals[1] - evals[0]
+
+
+    def detuning_noise_lab(self, ded):
+        """
+        Return the noise matrix for detuning noise in rad/ns
+        Input:
+          ded: dipolar detuning noise in GHz
+        Output:
+          (3x3) array in rad/ns
+        """
+        noise = np.array([[0, 0, 0], [0, 0, ded], [0, ded, 0]])
+        return 2*math.pi*noise
+
 
 
 class IdealCQ(CQ):
